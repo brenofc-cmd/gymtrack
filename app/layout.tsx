8d5f3967708a6b304458b68f61d1826dabd8e02b
Brendon Castellani <175517016+brenofc-cmd@users.noreply.github.com>
2026-07-14 09:46:48 -0300

    Permitir zoom na interface

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,6 @@ export const viewport: Viewport = {
   themeColor: '#0B0D10',
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 }
 
 export default function RootLayout({
